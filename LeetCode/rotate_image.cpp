eb35c900b0f6456318da2b11bbd48e89118bfaec
ShishirMagge <75026311+ShishirMagge@users.noreply.github.com>
2023-06-08 23:21:48 +0530

    Create rotate_image.cpp

---
+++ LeetCode/rotate_image.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(), matrix.end());
+
+        for(int row=0; row<matrix.size(); row++){
+            for(int col = row+1; col<matrix[0].size(); col++){
+                swap(matrix[row][col], matrix[col][row]);
+            }
+        }
+    }
+};
